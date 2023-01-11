8e8247413cd91f31e4f422adef0985c9b375927f
Breno R.Cunha <breno.rcunha2@gmail.com>
2023-01-11 10:41:11 -0300

    Reformas 11-1

--- htmlPizzas/javascript.js
+++ htmlPizzas/javascript.js
@@ -1,75 +1,88 @@
-'use strict';
-//Para que o navegador veja se está compatível com ele as infos
+(function main(win,doc){
+	'use strict';
+    //Para fins de renderização.
+	let btVendas = document.getElementById('vendas');
+	let btEstorno = document.getElementById('estorno');
 
-let somaMussarela = 0;
-let somaPortuguesa = 0;
-let somaCalabresa = 0;
-let vendaMussarela = 0.00;
-let vendaPortuguesa = 0.00;
-let vendaCalabresa = 0.00;
+	btVendas.addEventListener('click',venda,false);
+	btEstorno.addEventListener('click',estorno,false);
 
-let btnVendas = document.getElementById('vendas');
-let btnEstorno = document.getElementById('estorno');
-let btnPlanilha = document.getElementById('export');
 
-//Eventos:
-btnVendas.addEventListener('click',venda, false);
-btnEstorno.addEventListener('click',estorno, false);
-btnPlanilha.addEventListener('click',planilha,false);
+	let somaMussarela = 0;
+	let somaMista = 0;
+	let somaCalabresa = 0;
+	let vendaMussarela = 0;
+	let vendaMista = 0;
+	let vendaCalabresa = 0;
 
-//Funcoes:
-function venda(e){
-    if(document.getElementById('mussarela').checked){
-        somaMussarela+=1;
-        vendaMussarela+=25.00;        
-        document.getElementById('resultadoMussarela').innerHTML = somaMussarela;
-        document.getElementById('vendaMussarela').innerHTML = vendaMussarela.toFixed(2);
-    }else if(document.getElementById('portuguesa').checked){
-        somaPortuguesa+=1;
-        vendaPortuguesa+=27.00;
-        document.getElementById('resultadoPortuguesa').innerHTML = somaPortuguesa;
-        document.getElementById('vendaPortuguesa').innerHTML = vendaPortuguesa.toFixed(2);
-    } else if(document.getElementById('calabresa').checked){
-        somaCalabresa+=1;
-        vendaCalabresa+=25.00;
-        document.getElementById('resultadoCalabresa').innerHTML = somaCalabresa;
-        document.getElementById('vendaCalabresa').innerHTML = vendaCalabresa.toFixed(2);
-    } 
-}
+	function venda(e) 
+	{
+	    if(document.getElementById('mussarela').checked)
+	    {
+            somaMussarela = somaMussarela + 1;
+		    vendaMussarela = vendaMussarela + 25;
+		    document.getElementById("resultadoMussarela").innerHTML = somaMussarela;
+		    document.getElementById("vendaMussarela").innerHTML = vendaMussarela;
+	    }else if(document.getElementById("mista").checked)
+	    {
+		    somaMista = somaMista + 1;
+		    vendaMista = vendaMista + 25;
+		    document.getElementById("resultadoMista").innerHTML = somaMista;
+		    document.getElementById("vendaMista").innerHTML = vendaMista;
+	    }else if(document.getElementById('calabresa').checked)
+	    {
+		    somaCalabresa = somaCalabresa + 1;
+		    vendaCalabresa = vendaCalabresa + 25;
+		    document.getElementById("resultadoCalabresa").innerHTML = somaCalabresa;
+		    document.getElementById("vendaCalabresa").innerHTML = vendaCalabresa;
+	    }
+	}
 
-function estorno(e){
-    if(document.getElementById('mussarela').checked){
-        somaMussarela-=1;
-        vendaMussarela-=25.00;
-        if(somaMussarela <=0){
-            somaMussarela=0;
-            vendaMussarela=0.00;
-        }
-        document.getElementById('resultadoMussarela').innerHTML = somaMussarela;
-        document.getElementById('vendaMussarela').innerHTML = vendaMussarela.toFixed(2);
-    }else if(document.getElementById('portuguesa').checked){
-        somaPortuguesa-=1;
-        vendaPortuguesa-=27.00;
-        if(somaPortuguesa <=0){
-            somaPortuguesa=0;
-            vendaPortuguesa=0.00;
-        }
-        document.getElementById('resultadoPortuguesa').innerHTML = somaPortuguesa;
-        document.getElementById('vendaPortuguesa').innerHTML = vendaPortuguesa.toFixed(2);
-    } else if(document.getElementById('calabresa').checked){
-        somaCalabresa-=1;
-        vendaCalabresa-=25.00;
-        if(somaCalabresa <=0){
-            somaCalabresa=0;
-            vendaCalabresa=0.00;
-        }
-        document.getElementById('resultadoCalabresa').innerHTML = somaCalabresa;
-        document.getElementById('vendaCalabresa').innerHTML = vendaCalabresa.toFixed(2);
-    } 
-}
-function planilha(e){
-    TableToExcel.convert(document.getElementById('table')); 
-}
+	function estorno(e) 
+	{
+	    if(document.getElementById('mussarela').checked)
+	    {
+		    somaMussarela = somaMussarela - 1;
+		    vendaMussarela = vendaMussarela - 25;
+            if(somaMussarela<0){
+                somaMussarela = 0;
+                vendaMussarela=0.00;
+            }
+		    document.getElementById("resultadoMussarela").innerHTML = somaMussarela;
+		    document.getElementById("vendaMussarela").innerHTML = vendaMussarela;
+	    }else if(document.getElementById("mista").checked)
+	    {
+		    somaMista = somaMista - 1;
+		    vendaMista = vendaMista - 25;
+            if(somaMista<0){
+                somaMista = 0;
+                vendaMista = 0.00;
+            }
+		    document.getElementById("resultadoMista").innerHTML = somaMista;
+		    document.getElementById("vendaMista").innerHTML = vendaMista;
+	    }else if(document.getElementById('calabresa').checked)
+	    {
+		    somaCalabresa = somaCalabresa - 1;
+		    vendaCalabresa = vendaCalabresa - 25;
+            if(somaCalabresa<0){
+                somaCalabresa = 0;
+                vendaCalabresa = 0.00;
+            }
+		    document.getElementById("resultadoCalabresa").innerHTML = somaCalabresa;
+		    document.getElementById("vendaCalabresa").innerHTML = vendaCalabresa;
+	    }
+	}
+
+	function exportReportToExcel() {
+        TableToExcel.convert(document.getElementById("table"), {
+	    name: "planilha.xlsx",
+	    sheet: {
+		name: "planilha"
+	    }
+	    });
+	}
+})(window,document);
+
 
 /*CALCULADORA SIMPLES: 
 const x = prompt("Informe um numero: ");
