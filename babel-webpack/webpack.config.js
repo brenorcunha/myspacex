f1ed19258b71568497b162f5fc2cf000fe90c8af
Breno R.Cunha <breno.rcunha2@gmail.com>
2023-12-14 10:26:47 -0300

    webpack&babelintegration

---
+++ babel-webpack/webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output:{
+    filename: '[name].min.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.js$/,
+      use: ['babel-loader']
+    }]
+  },
+}
